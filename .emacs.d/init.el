37c7b6f880f25909220aa991be8f51d6508915cf
Matt Sanders <matt8@cisco.com>
2022-01-10 10:40:52 +1100

    company, magit

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -66,7 +66,7 @@
   (setq lsp-keymap-prefix "C-c l")
   :bind ("M-RET" . completion-at-point)
   :hook
-  ((c-mode c++-mode) . lsp)
+  ;; ((c-mode c++-mode) . lsp)
   (verilog-mode . lsp))
 
 ;; if you are ivy user
@@ -250,3 +250,23 @@
   :config
   (when (memq window-system '(mac ns x))
     (exec-path-from-shell-initialize)))
+
+(use-package magit
+  :commands magit-status
+  :bind ("C-c g" . magit-status)
+  :custom
+  (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
+
+(use-package company
+  :after lsp-mode
+  :hook (lsp-mode . company-mode)
+  :bind (:map company-active-map
+         ("<tab>" . company-complete-selection))
+        (:map lsp-mode-map
+         ("<tab>" . company-indent-or-complete-common))
+  :custom
+  (company-minimum-prefix-length 1)
+  (company-idle-delay 0.0))
+
+(use-package company-box
+  :hook (company-mode . company-box-mode))
